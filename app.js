81e1f07a04b84e5d061e3d3a0758f4f7661cd7d8
Mohamed Mursal <mursalmohamed02@gmail.com>
2022-04-21 00:47:46 +0100

    Update: learnt about hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require ("ejs");
 const mongoose = require ("mongoose")
-const encrypt = require ("mongoose-encryption");
+
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 
 const User = mongoose.model("User", userSchema);
@@ -48,7 +49,7 @@ app.get("/register", function (req, res){
 app.post("/register", function(req,res){
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -62,7 +63,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function (err, foundUser){
         if (err){
@@ -70,7 +71,6 @@ app.post("/login", function(req, res){
         } else {
             if (foundUser) {
                 if (foundUser.password === password){
-                    console.log(foundUser.password);
                     res.render("secrets");
                 }
             }
